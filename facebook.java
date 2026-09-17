cadd550b130bce6febd929191f95d2af464a210a
root <root@ip-172-31-6-96.ap-northeast-3.compute.internal>
2026-09-17 07:32:06 +0000

    This is my second commit

--- facebook.java
+++ facebook.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;  
+This is m GIT Session...!
 import java.lang.Math;  
 import java.io.printer; 
 import java.io.collection; 
@@ -17,6 +17,25 @@ for(int i = 1; i <= size; i++)
 {  
 //outer loop      
 for(int j = 1; j <= size; j++)  
+{  
+//calculates and prints the values for pattern  
+System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");  
+}  
+System.out.println();  
+}  
+}  
+//driver code  
+public static void main(String args[])  
+{  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the value of n: ");  
+int n = sc.nextInt();  
+System.out.println();  
+//function calling  
+printPattern(n);  
+}  
+}
+
 {  
 //calculates and prints the values for pattern  
 System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");  
